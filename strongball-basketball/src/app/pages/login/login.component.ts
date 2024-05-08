1e5d220beb28b50ce81cfecfe568ed54fa81099a
Kovács Gilbert <h160307@stud.u-szeged.hu>
2024-05-08 03:16:23 +0200

    feat: Create login page

--- strongball-basketball/src/app/pages/login/login.component.ts
+++ strongball-basketball/src/app/pages/login/login.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  
+  constructor(private router: Router){
 
+  }
+
+  login(){
+    
+  }
 }
