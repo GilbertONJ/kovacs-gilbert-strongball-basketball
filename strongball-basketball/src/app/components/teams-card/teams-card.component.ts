05817e4204ffefcdc9072507dd17c797538adeb0
Kovács Gilbert <h160307@stud.u-szeged.hu>
2024-05-08 03:07:14 +0200

    feat: teams pop-up

--- strongball-basketball/src/app/components/teams-card/teams-card.component.ts
+++ strongball-basketball/src/app/components/teams-card/teams-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-teams-card',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './teams-card.component.scss'
 })
 export class TeamsCardComponent {
+  @Input() public full_name: string = '';
+  @Input() public helyezes: string = '';
+  @Input() public lose: number = 0;
+  @Input() public win: number = 0;
+  @Input() public best_player: string = '';
+  
+
+  constructor(public readonly activeModal: NgbActiveModal) {}
 
 }
