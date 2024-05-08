7e02c99f8175606357f8a0be46c86ba29afb7723
Kovács Gilbert <h160307@stud.u-szeged.hu>
2024-05-08 03:21:26 +0200

    feat: create responsive navbar

--- strongball-basketball/src/app/app-routing.module.ts
+++ strongball-basketball/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'sign-up', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) }];
-
+const routes: Routes = [
+  { 
+    path: 'home', loadChildren: () => import('../app/pages/home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    path: '', loadChildren: () => import('../app/pages/login/login.module').then(m => m.LoginModule) 
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'signUp', loadChildren: () => import('./pages/sign-up/sign-up.module').then(m => m.SignUpModule) }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
