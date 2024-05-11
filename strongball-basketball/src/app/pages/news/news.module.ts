b6844e745ce6a35cd08746fb6bd5da803734dc09
Kovács Gilbert <h160307@stud.u-szeged.hu>
2024-05-11 21:14:10 +0200

    feat: add-edit component

--- strongball-basketball/src/app/pages/news/news.module.ts
+++ strongball-basketball/src/app/pages/news/news.module.ts
@@ -5,16 +5,21 @@ import { NewsRoutingModule } from './news-routing.module';
 import { NewsComponent } from './news.component';
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { AddEditComponent } from '../../components/add-edit/add-edit.component';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 
 @NgModule({
   declarations: [
-    NewsComponent
+    NewsComponent,
+    AddEditComponent,
   ],
   imports: [
     CommonModule,
     NewsRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ]
 })
 export class NewsModule { }
